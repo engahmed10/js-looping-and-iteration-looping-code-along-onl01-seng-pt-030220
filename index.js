b9ab42e663be68a3329e6a279cde182984a366be
AHMED AFANDY <eng.computer1@yahoo.com>
2020-08-24 01:30:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Code your solutions in this file
 function writeCards(array){
+<<<<<<< HEAD
 let arr=[]
 
 for(let index=0; index < array.length; index++)
@@ -17,4 +18,12 @@ while(count >= 0 )
  console.log(count--)
 }
 
+=======
+const arr=[]
+for(let index=0; index < array.length; index++)
+{
+   arr.push 'Thank you, ${array[index]}'
+}
+console.log(arr)
+>>>>>>> f7f27878fbff4a1bba3c4a190504a39fd9e8c9b7
 }
